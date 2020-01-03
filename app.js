9e79c3561fa2c409819ac5cb30420e6aa581657c
atcanbolat <kangalcanbolat@gmail.com>
2020-01-03 15:11:30 -0800

    added lib file with employee classes

---
+++ app.js
@@ -0,0 +1,11 @@
+const inquirer = require("inquirer");
+const jest = require("jest");
+
+
+// constructor function which can take in a series of values and create objects
+// with the properties contained inside
+
+
+
+
+
